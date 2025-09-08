9d2c25ef497575fa6e47894ece61e11a15000f7e
Karandas333 <146736956+Karandas333@users.noreply.github.com>
2025-09-08 18:45:34 +0530

    Update client.js

--- client.js
+++ client.js
@@ -1,15 +1,21 @@
-const { createClient } = require('redis');
+const redis = require("redis");
 
-const client = createClient({
-    username: 'default',
-    password: process.env.REDIS_PASSWORD,
-    socket: {
-        host: process.env.REDIS_HOST,
-        port: process.env.REDIS_PORT
-    }
+const client = redis.createClient({
+  socket: {
+    host: process.env.REDIS_HOST || "127.0.0.1",
+    port: process.env.REDIS_PORT || 6379,
+  },
+  password: process.env.REDIS_PASSWORD || undefined,
 });
 
-client.on('error', err => console.log('Redis Client Error', err));
+client.on("error", (err) => {
+  console.error("Redis Client Error", err);
+});
+
+(async () => {
+  await client.connect();
+  console.log("✅ Redis connected!");
+})();
 
 module.exports = client;
 
